d06db8d64bdba432f31bf87ab60d8799a56493d9
Alfred Huang <whatever0426.am03@gmail.com>
2024-10-27 01:24:13 +0800

    add markdown

--- src/app/ShowMarkdown.tsx
+++ src/app/ShowMarkdown.tsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import React from "react";
+import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
+import rehypeRaw from "rehype-raw";
+
+const markdown = `
+# Hello World
+
+This is a **bold** text with a link to [GitHub](https://github.com).
+
+## Task List
+- [x] Task 1
+- [ ] Task 2
+`;
 
 export const ShowMarkdown = () => {
   return (
-    <div>ShowMarkdown</div>
-  )
-}
+    <>
+      <ReactMarkdown remarkPlugins={[remarkGfm]} rehypePlugins={[rehypeRaw]}>
+        {markdown}
+      </ReactMarkdown>
+    </>
+  );
+};
