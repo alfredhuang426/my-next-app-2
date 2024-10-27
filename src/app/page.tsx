82e0d0d1757bac24513fb351075ea40dd7241918
Alfred Huang <whatever0426.am03@gmail.com>
2024-10-27 16:33:54 +0800

    xdd

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import styles from "./page.module.css";
-import { ShowMarkdown } from "./ShowMarkdown";
+// import { ShowMarkdown } from "./ShowMarkdown";
 
 export default function Home() {
   return (
     <div className={styles.page}>
-      <ShowMarkdown />
+      {/* <ShowMarkdown /> */}
+      xddd
     </div>
   );
 }
